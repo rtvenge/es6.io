30d4b51df88a9b0be5357c8d85ae6b9164fe2488
Ryan Tvenge <ryan@hoverboardstudios.com>
2017-03-21 09:45:08 -0700

    messing with config file

--- 13 - JavaScript Modules and Using npm/es6modules/app.js
+++ 13 - JavaScript Modules and Using npm/es6modules/app.js
@@ -1,7 +1,18 @@
 import { uniq } from 'lodash';
 import jquery from 'jquery';
 import jsonp from 'jsonp';
+import { apiKey as key,
+  url,
+  sayHi,
+  age,
+  dog
+} from './src/config';
 
-const ages = [1,1,4,52,12,4];
+console.log(key);
+console.log(url);
+
+sayHi('Ryan');
+
+const ages = [1, 1, 4, 52, 12, 4];
 
 console.log(uniq(ages));
---
+++ 13 - JavaScript Modules and Using npm/es6modules/src/config.js
@@ -0,0 +1,12 @@
+export const apiKey = 'abc123';
+
+export const url = 'https://ryantvenge.com';
+
+export function sayHi(name) {
+  console.log(`hello there ${name}!`);
+}
+
+const age = 100;
+const dog = 'Choc';
+
+export { age, dog };
