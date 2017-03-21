58d7f2d3fdced9a228b5818c46e054f052dbd3a2
Ryan Tvenge <ryan@hoverboardstudios.com>
2017-03-21 14:03:46 -0700

    working with babel

--- 14 - ES6 Tooling/babel/app-compiled.js
+++ 14 - ES6 Tooling/babel/app-compiled.js
@@ -2,11 +2,10 @@
 
 function _objectWithoutProperties(obj, keys) { var target = {}; for (var i in obj) { if (keys.indexOf(i) >= 0) continue; if (!Object.prototype.hasOwnProperty.call(obj, i)) continue; target[i] = obj[i]; } return target; }
 
-var _x$y$a$b = { x: 1, y: 2, a: 3, b: 4 };
-var x = _x$y$a$b.x;
-var y = _x$y$a$b.y;
-
-var z = _objectWithoutProperties(_x$y$a$b, ['x', 'y']);
+var _x$y$a$b = { x: 1, y: 2, a: 3, b: 4 },
+    x = _x$y$a$b.x,
+    y = _x$y$a$b.y,
+    z = _objectWithoutProperties(_x$y$a$b, ['x', 'y']);
 
 var age = 100;
 var people = ['Wes', 'Kait'];
